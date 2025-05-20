81be14846308e66a3aa508901f176a887e1bd462
Kartik Chauhan <78919172+KartikChauhn@users.noreply.github.com>
2025-05-20 23:55:20 +0530

    Create OiAnalysisSchema.js

---
+++ OiAnalysisSchema.js
@@ -0,0 +1,24 @@
+const optionData = {
+  "2025-05-15T09:15:00+05:30": {
+    18000: {
+      ce: {
+        inStrikePrice: 18000,
+        stTime: "09:15-15:30",
+        stDataFetchType: "IM", // Intraday
+        inOi: 1680,
+        inClose: 1046.2,
+        inDayHigh: 1050,
+        inDayLow: 1000,
+      },
+      pe: {
+        inStrikePrice: 18000,
+        stTime: "09:15-15:30",
+        stDataFetchType: "EOD", // End of Day
+        inOi: 1645,
+        inClose: 1046.2,
+        inDayHigh: 1050,
+        inDayLow: 1000,
+      },
+    },
+  },
+};
